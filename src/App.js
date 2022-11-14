8c190540c44f74180f6c65697e16acc3e55025ff
sbalayan1 <balayans2014@yahoo.com>
2022-11-13 20:17:08 -0800

    update'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
 import React, {useState, useEffect} from 'react'
 import {ResponsiveContainer, BarChart, Bar, YAxis, XAxis, Tooltip, CartesianGrid} from 'recharts'
 
-import {bubbleSort} from './sortAlgos'
+import {bubbleSort, quickSort, convertData} from './sortAlgos'
 
 function App() {
   const [data, setData] = useState([])
+  const [sortMethod, setSortMethod] = useState("Quick Sort")
+  const [timeToComplete, setTimeToComplete] = useState(0)
+
+  
   const handleSort = () => {
-    setData(bubbleSort(data))
+    const start = window.performance.now()
+    const dataArr = data.map(obj => obj.value)
+    let sortedData; 
+    if (sortMethod === "Quick Sort") {
+      sortedData = quickSort(dataArr, 0, dataArr.length - 1)
+    }
+
+    if (sortMethod === "Bubble Sort") {
+      sortedData = bubbleSort(dataArr)
+      while (sortedData.find((val, index) => val > sortedData[index+1]) !== undefined) {
+        bubbleSort(dataArr)
+      }
+    }
+
+    const end = window.performance.now()
+    setTimeToComplete(end - start)
+    setData(convertData(sortedData))
+  }
+
+  const handleReset = () => {
+    let array = []
+    while (array.length < 10) {
+      const obj = {}
+      obj["index"] = array.length
+      obj["value"] = Math.floor(Math.random()* 10) + 1
+      array.push(obj)
+    }
+
+    console.log('state set')
+    setTimeToComplete(0)
+    setData(array)
   }
 
   useEffect(() => {
-    setData(data => {
-      let array = [...data]
-      let count = 10
-      while (count > 0) {
-        const obj = {}
-        obj["index"] = 10 - count
-        obj["value"] = Math.floor(Math.random()* 10) + 1
-        array.push(obj)
-        count --
-      }
-      console.log('state set')
-      return array
-    })
+    handleReset()
   }, [])
 
   return (
     <>
+     <h4>Time to complete: {timeToComplete}ms</h4>
       <ResponsiveContainer height={500} width="50%">
         <BarChart data={data}>
           <XAxis dataKey="index"/>
@@ -36,7 +59,13 @@ function App() {
           <Bar dataKey="value" stroke="#344F74" fill="#ba000d"/>
         </BarChart>
       </ResponsiveContainer>
+      <select onChange={(e) => setSortMethod(e.target.value)}>
+        <option value="Quick Sort">Quick Sort</option>
+        <option value="Bubble Sort">Bubble Sort</option>
+      </select>
       <button onClick={handleSort}>Sort</button>
+      <button onClick={handleReset}>Reset</button>
+
     </>
   );
 }
--- src/sortAlgos.js
+++ src/sortAlgos.js
@@ -7,18 +7,42 @@ function convertData(arr) {
 }
 
 function bubbleSort(arr) {
-    let dataArr = arr.map(obj => obj.value)
-    for (let i = 0; i<dataArr.length; i++) {
+    // let dataArr = arr.map(obj => obj.value)
+    for (let i = 0; i<arr.length; i++) {
         let j = i+1
-        while (dataArr[i] > dataArr[j]) {
-            [dataArr[i], dataArr[j]] = [dataArr[j], dataArr[i]]
+        while (arr[i] > arr[j]) {
+            [arr[i], arr[j]] = [arr[j], arr[i]]
             j++
         }
+    }
+
+    return arr
+}
+
+function partition(arr, start, end) {
+    let pivot = arr[start]
+    let swapIdx = start
+
+    for (let i = start+1; i<=end; i++) {
+        if (arr[i] < pivot) {
+            swapIdx++
+            [arr[swapIdx], arr[i]] = [arr[i], arr[swapIdx]]
+        }
+    }
+
+    [arr[start], arr[swapIdx]] = [arr[swapIdx], arr[start]]
+    return swapIdx
+}
+
+function quickSort(arr, left, right) {
+    if (left < right) {
+        let pivotIndex = partition(arr, left, right)
+        quickSort(arr, left, pivotIndex - 1)
+        quickSort(arr, pivotIndex + 1, right)
 
     }
 
-    console.log(convertData(dataArr))
-    return convertData(dataArr)
+    return arr
 }
 
-export {bubbleSort}
+export {bubbleSort, quickSort, convertData}
